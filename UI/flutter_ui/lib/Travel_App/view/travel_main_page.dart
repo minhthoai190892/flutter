46c26de6dbdb6647702329bf0755d7109f4d4451
minhthoai <minhthoai190892@gmail.com>
2024-06-10 23:39:18 +0700

    Travel_App-part-3

--- UI/flutter_ui/lib/Travel_App/view/travel_main_page.dart
+++ UI/flutter_ui/lib/Travel_App/view/travel_main_page.dart
@@ -1,16 +1,162 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
 
-class TravelMainPage extends StatelessWidget {
+class TravelMainPage extends StatefulWidget {
   const TravelMainPage({super.key});
 
   @override
+  State<TravelMainPage> createState() => _TravelMainPageState();
+}
+
+class _TravelMainPageState extends State<TravelMainPage> {
+  int pageIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Column(
-        children: [],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          border: Border.all(color: Colors.grey)),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search',
+                          suffixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Gap(16),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(32),
+                        border: Border.all(color: Colors.grey)),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.filter_alt_outlined)),
+                  ),
+                ],
+              ),
+              const Gap(16),
+              SizedBox(
+                height: 42,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(32),
+                        color: const Color.fromRGBO(124, 132, 100, 1),
+                      ),
+                      child: const Text(
+                        'Culture',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    const Gap(16),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black)),
+                      child: const Text(
+                        'Culture',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Gap(16),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black)),
+                      child: const Text(
+                        'Culture',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Gap(16),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black)),
+                      child: const Text(
+                        'Culture',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Gap(16),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          color: Colors.white,
+                          border: Border.all(color: Colors.black)),
+                      child: const Text(
+                        'Culture',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Gap(16),
+                  ],
+                ),
+              ),
+              const Gap(16),
+              Expanded(child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return const Placeholder();
+                },
+              ))
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
+        currentIndex: pageIndex,
+        onTap: (value) {
+          setState(() {
+            pageIndex = value;
+          });
+        },
+        selectedItemColor: const Color.fromRGBO(124, 132, 100, 1),
+        backgroundColor: Colors.white,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.map_outlined), label: 'Map'),
