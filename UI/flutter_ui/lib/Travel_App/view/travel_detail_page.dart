5f3b64c98260dd3f55adfac4db886f32853e0f29
minhthoai <minhthoai190892@gmail.com>
2024-06-11 23:32:40 +0700

    Travel_App-part-4

---
+++ UI/flutter_ui/lib/Travel_App/view/travel_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TravelDetailPage extends StatefulWidget {
+  const TravelDetailPage({super.key});
+
+  @override
+  State<TravelDetailPage> createState() => _TravelDetailPageState();
+}
+
+class _TravelDetailPageState extends State<TravelDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- UI/flutter_ui/lib/Travel_App/view/travel_main_page.dart
+++ UI/flutter_ui/lib/Travel_App/view/travel_main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_ui/Travel_App/view/travel_detail_page.dart';
 import 'package:gap/gap.dart';
 
 class TravelMainPage extends StatefulWidget {
@@ -138,11 +139,86 @@ class _TravelMainPageState extends State<TravelMainPage> {
                 ),
               ),
               const Gap(16),
-              Expanded(child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return const Placeholder();
-                },
-              ))
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => const TravelDetailPage(),
+                      )),
+                      child: Container(
+                        height: 320,
+                        margin: const EdgeInsets.only(bottom: 24),
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(16),
+                          color: Colors.green,
+                          image: const DecorationImage(
+                              image: NetworkImage(
+                                  'https://images.unsplash.com/photo-1596517447907-4393f30b1b7a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8eGpQUjRobGtCR0F8fGVufDB8fHx8fA%3D%3D'),
+                              fit: BoxFit.cover),
+                        ),
+                        child: Column(
+                          children: [
+                            const Row(
+                              children: [
+                                Spacer(),
+                                CircleAvatar(
+                                  radius: 24,
+                                  backgroundColor: Colors.white,
+                                  child: Icon(Icons.favorite_border_outlined),
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Culture'),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Serect Temple',
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      Spacer(),
+                                      Icon(Icons.star_border),
+                                      Text('4.5')
+                                    ],
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(
+                                        Icons.location_on_outlined,
+                                        size: 18,
+                                      ),
+                                      Text('Unknown location'),
+                                      Spacer(),
+                                      Text(
+                                        '\$147',
+                                        style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
