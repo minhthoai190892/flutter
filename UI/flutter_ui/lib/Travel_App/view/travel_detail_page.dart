eda89e3ce6cc40d5ad2b06d85b7b00ccfb4ae060
minhthoai <minhthoai190892@gmail.com>
2024-06-12 23:36:36 +0700

    Travel_App-part-5

--- UI/flutter_ui/lib/Travel_App/view/travel_detail_page.dart
+++ UI/flutter_ui/lib/Travel_App/view/travel_detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
 
 class TravelDetailPage extends StatefulWidget {
   const TravelDetailPage({super.key});
@@ -10,6 +12,113 @@ class TravelDetailPage extends StatefulWidget {
 class _TravelDetailPageState extends State<TravelDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200], shape: BoxShape.circle),
+                    child: const Icon(Icons.arrow_back),
+                  ),
+                  const Expanded(
+                      child: Center(
+                    child: Text('Detail'),
+                  )),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey[200], shape: BoxShape.circle),
+                    child: const Icon(Icons.favorite_outline),
+                  ),
+                ],
+              ),
+              const Gap(24),
+              Expanded(
+                  child: SingleChildScrollView(
+                child: DefaultTabController(
+                  length: 2,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 420,
+                        decoration: const BoxDecoration(color: Colors.grey),
+                      ),
+                      const TabBar(
+                          isScrollable: true,
+                          tabAlignment: TabAlignment.start,
+                          tabs: [
+                            Tab(
+                              text: 'Description',
+                            ),
+                            Tab(
+                              text: 'Review',
+                            )
+                          ]),
+                      const Gap(16),
+                      const SizedBox(
+                        height: 400,/*  */
+                        child: TabBarView(children: [
+                          Text(
+                              'Lorem ipsum dolor sit amet\n consectetur adipisicing elit.'),
+                          Text(
+                              'Lorem ipsum dolor sit amet\n consectetur adipisicing elitdddddddddddddddddddddddddddddddddddddddddd/*  */.'),
+                        ]),
+                      )
+                    ],
+                  ),
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        height: 80,
+        color: Colors.white,
+        surfaceTintColor: Colors.white,
+        child: Row(
+          children: [
+            const Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Start From',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '\$175',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(text: '/Person')
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              decoration: BoxDecoration(
+                color: const Color.fromRGBO(124, 132, 100, 1),
+                borderRadius: BorderRadius.circular(27),
+              ),
+              child: const Text(
+                'Book now',
+                style: TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
