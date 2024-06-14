1f2279b8bc66c2e25196e14b1d29ea33d9e3f541
minhthoai <minhthoai190892@gmail.com>
2024-06-15 06:58:47 +0700

    Hiking_Guide-part-3

--- UI/flutter_ui/lib/Hiking_Guide/views/hiking_guide_app.dart
+++ UI/flutter_ui/lib/Hiking_Guide/views/hiking_guide_app.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
@@ -53,34 +54,118 @@ class HikingGuideApp extends StatelessWidget {
                 ),
                 child: const TextField(
                   decoration: InputDecoration(
-                    border: InputBorder.none,
-                    icon: Icon(Icons.search),
-                  ),
+                      border: InputBorder.none,
+                      icon: Icon(Icons.search),
+                      hintText: 'Search route mount,....'),
                 ),
               ),
               const Gap(16),
-              const SizedBox(
-                height: 24,
-                child: Placeholder(),
+              SizedBox(
+                height: 32,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 16),
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey[300]!),
+                          borderRadius: BorderRadius.circular(24)),
+                      child: const Row(
+                        children: [
+                          Text('Sort'),
+                          Icon(Icons.keyboard_arrow_down_sharp)
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
+              const Gap(24),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text('Community Event'),
+                  const Text(
+                    'Community Event',
+                    style: TextStyle(fontSize: 16),
+                  ),
                   TextButton(onPressed: () {}, child: const Text('See more'))
                 ],
               ),
               Container(
-                height: 160,
-                decoration: BoxDecoration(border: Border.all()),
+                padding: const EdgeInsets.all(8),
+                height: 200,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey[400]!),
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                        'Lorem ipsum dolor sit, amet consectetur adipisicing elit.'),
+                    const Gap(6),
+                    const Row(
+                      children: [
+                        Icon(Icons.calendar_month),
+                        Text('22/2/2022'),
+                        Icon(Icons.golf_course),
+                        Text('Magelang'),
+                      ],
+                    ),
+                    const Gap(6),
+                    const Text(
+                      '  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur est laboriosam beatae exercitationem facilis sit, odio unde reprehenderit libero animi cum labore assumenda, perspiciatis culpa. Illum itaque reiciendis recusandae repellat?',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const Gap(12),
+                    Row(
+                      children: [
+                        const CircleAvatar(),
+                        const CircleAvatar(),
+                        const CircleAvatar(),
+                        const Text('Dream, +8 more'),
+                        const Spacer(),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 12),
+                          decoration:
+                              const BoxDecoration(color: Colors.lightGreen),
+                          child: const Center(
+                            child: Text(
+                              'Join Event',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Text('Arrow your location'),
+                  const Text(
+                    'Arrow your location',
+                    style: TextStyle(fontSize: 16),
+                  ),
                   TextButton(onPressed: () {}, child: const Text('See more'))
                 ],
               ),
+              Expanded(
+                  child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 320,
+                    margin: const EdgeInsets.only(right: 8),
+                    decoration: const BoxDecoration(color: Colors.blueAccent),
+                  );
+                },
+              ))
             ],
           ),
         ),
--- UI/flutter_ui/lib/Hiking_Guide/views/hiking_login_page.dart
+++ UI/flutter_ui/lib/Hiking_Guide/views/hiking_login_page.dart
@@ -18,6 +18,11 @@ class HikingLoginPage extends StatelessWidget {
             flex: 7,
             child: Container(
               decoration: const BoxDecoration(color: Colors.red),
+              child: Image.network(
+                width: double.infinity,
+                'https://images.unsplash.com/photo-1572598973323-eeef47c14431?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDl8RnpvM3p1T0hONnd8fGVufDB8fHx8fA%3D%3D',
+                fit: BoxFit.cover,
+              ),
             ),
           ),
           Expanded(
