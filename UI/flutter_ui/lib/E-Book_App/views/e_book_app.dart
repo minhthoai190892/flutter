4a4dc76cdfd17995d65350d3b7f238b8e41104c7
minhthoai <minhthoai190892@gmail.com>
2024-07-04 23:11:22 +0700

    e-book_App-part-1

--- UI/flutter_ui/lib/E-Book_App/views/e_book_app.dart
+++ UI/flutter_ui/lib/E-Book_App/views/e_book_app.dart
@@ -1,39 +1,195 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
 
-class EBookApp extends StatelessWidget {
+class EBookApp extends StatefulWidget {
   const EBookApp({super.key});
 
   @override
+  State<EBookApp> createState() => _EBookAppState();
+}
+
+class _EBookAppState extends State<EBookApp> {
+  int menuIndex = 0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            // appbar
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                const Icon(Icons.app_registration_rounded),
-                Expanded(
-                  child: Container(
-                    decoration: const ShapeDecoration(
-                      shape: StadiumBorder(),
+                // appbar
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Icon(Icons.app_registration_rounded),
+                    Expanded(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        decoration: const ShapeDecoration(
+                          color: Colors.white,
+                          shape: StadiumBorder(),
+                        ),
+                        child: const TextField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Search',
+                              suffixIcon: Icon(Icons.mic),
+                              prefixIcon: Icon(Icons.search_outlined)),
+                        ),
+                      ),
                     ),
-                  
+                    const Icon(Icons.notification_add),
+                    const Icon(Icons.shopping_bag),
+                  ],
+                ),
+                const Gap(12),
+                // top authors
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  height: 180,
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Top Author',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.circular(10)),
+                            child: const Row(
+                              children: [
+                                Text('See all'),
+                                Icon(Icons.arrow_forward_ios),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      const Divider(),
+                      const Gap(10),
+                      SizedBox(
+                        height: 100,
+                        child: ListView.separated(
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return const Column(
+                                children: [
+                                  CircleAvatar(),
+                                  Text('data'),
+                                ],
+                              );
+                            },
+                            separatorBuilder: (context, index) =>
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                            itemCount: 10),
+                      )
+                    ],
                   ),
                 ),
-                const Icon(Icons.notification_add),
-                const Icon(Icons.shopping_bag),
+                const Gap(15),
+                // menu
+                SizedBox(
+                    height: 40,
+                    child: ListView.separated(
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  menuIndex = index;
+                                });
+                              },
+                              child: Container(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                decoration: BoxDecoration(
+                                  color: menuIndex == index
+                                      ? Colors.amber[300]
+                                      : Colors.white,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(
+                                  child: Text('dataddd'),
+                                ),
+                              ),
+                            ),
+                        separatorBuilder: (context, index) => const SizedBox(
+                              width: 10,
+                            ),
+                        itemCount: 10)),
+                const Gap(15),
+                // top trending
+                Container(
+                  padding: const EdgeInsets.all(15),
+                  height: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text('Top Trending'),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.grey[200],
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: const Row(
+                              children: [
+                                Text('See All'),
+                                Icon(Icons.arrow_forward_ios)
+                              ],
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 150,
+                  child: Placeholder(),
+                ),
+                const SizedBox(
+                  height: 150,
+                  child: Placeholder(),
+                ),
+                const SizedBox(
+                  height: 150,
+                  child: Placeholder(),
+                ),
               ],
-            )
-            // top authors
-            // menu
-            // top trending
-          ],
+            ),
+          ),
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.amber,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          ]),
     );
   }
 }
